2e2c5d3b861aeaaebb2336aae0f0ef9017786876
wchoi32 <wchoi32@illinois.edu>
2018-01-16 20:51:05 -0800

    Some part of part 2 by WooSung Choi & Yalcin Odabasi

--- spec/part2.js
+++ spec/part2.js
@@ -58,7 +58,9 @@
       it('should return false given an array and a value not in that array', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var array = [1, 2, 3];
+        var value = 4;
+        expect(_.contains(array, value)).to.be.false;
       });
 
       it('should return true given a object and a value from that object', function() {
@@ -91,7 +93,7 @@
       it('fails for a collection of all-falsy values', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        expect(_.every([false, -1])).to.be.false;
       });
 
       it('fails for a collection containing mixed falsy and truthy values', function() {
@@ -149,7 +151,7 @@
       it('should fail for a set containing no matching values', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        expect(_.some([null, 0], _.identity)).to.be.false;
       });
 
       it('should pass for a collection containing one matching value', function() {
--- src/underbar.js
+++ src/underbar.js
@@ -203,12 +203,24 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || _.identity;
+    return _.reduce(collection, function(int, item) {
+      return int && !!iterator(item);
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || _.identity;
+    return !(_.every(collection, function(item) {
+      if (!iterator(item)) {
+        return true; 
+      } else {
+        return false;
+      }
+    }));
   };
 
 
@@ -288,7 +300,6 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
-    return setTimeout(func, wait);
   };
 
 
